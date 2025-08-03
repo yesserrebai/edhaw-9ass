ef8728d57a183f263324c44e483866295104deb9
Mariem BenYounes <maryemby21@gmail.com>
2025-08-03 17:38:26 +0100

    feat: added dark/light mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 'use client';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import LocationPrompt from '../components/LocationPrompt';
 import OutageButton from '../components/OutageButton';
 import ReportChart from '../components/ReportChart';
 import ReportTable from '../components/ReportTable';
 import LocationsPieCharts from '../components/LocationsPieCharts';
 import {Location,Report} from '../types/types'
+import { useTheme } from 'next-themes';
+import { LightSwitchOn,LightSwitchOff } from '@/components/LightSwitches';
+
 
 export default function HomePage() {
   const [location, setLocation] = useState<Location | null>(null);
   const [reports, setReports] = useState<Report[]>([]);
+  const { theme, setTheme } = useTheme();
 
   const submitReport = (timeCategory: Report['timeCategory']) => {
     if (!location) return alert('Location not set');
@@ -22,22 +26,56 @@ export default function HomePage() {
 
     setReports((prev) => [...prev, report]);
   };
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setTheme(storedTheme);
+    } else {
+      setTheme('dark');
+    }
+  }, []);
+
+  const togglePower = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      setTheme('light');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 
   return (
-    <div className="container mx-auto p-4">
-      <div className='flex justify-between mb-4'>
-      <h1 className="text-2xl font-bold mb-4">
+    <div className="container mx-auto p-4 bg-white dark:bg-gray-900 text-black dark:text-white">
+      <div className='flex justify-between mb-[50px] border-b border-gray-700 dark:border-gray-200 pb-2'>
+      <h1 className="text-2xl font-bold pt-2">
        Edhaw 9as
       </h1>
-      <button
-        type="button"
-        className={`ml-2 px-3 py-2 rounded-full bg-gray-200 hover:bg-gray-300 active:bg-gray-400 transition ml-auto w-fit`}
-        aria-label="Power"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6 text-gray-700">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v9m6.364-4.364a9 9 0 11-12.728 0" />
-        </svg>
-       </button>
+      <div className="flex items-center gap-4">
+        <div className="flex items-center text-sm px-4 py-2 rounded-4xl bg-white p-2 border dark:text-black group transition-all duration-300">
+          <>
+            Edhaw yekhdem 3ana 😎
+            <span
+              className="ml-2 flex items-center transition-all duration-300 group-hover:scale-x-110 group-hover:translate-x-2 group-hover:text-gray-400"
+              style={{ transformOrigin: 'right' }}
+            >
+              {'>'}
+            </span>
+          </>
+        </div>
+        <button
+          type="button"
+          aria-label="Power"
+          onClick={togglePower}
+          className="focus:outline-none"
+        >
+          {theme === 'light' ? (
+        <LightSwitchOn size={48} className="hover:opacity-80" />
+          ) : (
+        <LightSwitchOff size={48} className="hover:opacity-80" />
+          )}
+        </button>
+      </div>
       </div>
       <LocationPrompt onLocationSet={setLocation} location={location} />
       {location && <OutageButton onSelect={submitReport} />}
